ae7ffe86d1cc843ea7894f37666e5229b297ccca
Guilherme Barros <wingui3@gmail.com>
2023-04-10 21:32:47 -0300

    :sparkles: Atualizando dados

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withImages = require('next-images');
 
 module.exports = withImages({
-  esModule: true
+  esModule: true,
+  webpack5: false
 });
--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
 import { CodeItem, Container, InfosContainer, TextContainer } from './styles';
 import picture from '../../assets/pessoa.webp';
@@ -13,23 +14,23 @@ function HomeHero() {
         </TextContainer>
         <InfosContainer>
           <CodeItem>
-            <span className="comment">//Minha apresentação</span>
+            <span className="comment">// Minha apresentação</span>
             <span className="purple">Infos</span> {'\u007D'}
             <div>
-              Nome:<span className="blue">Guilherme,</span>
+              Nome:<span className="blue"> Guilherme,</span>
             </div>
             <div>
-              Sobrenome:<span className="blue">Barros</span>
+              Sobrenome:<span className="blue"> Barros</span>
             </div>
             {'\u007D'}
           </CodeItem>
           <CodeItem>
             <span className="purple">Cargo</span> {'\u007D'}
             <div>
-              Função:<span className="blue">Desenvolvedor</span>
+              Função:<span className="blue"> Dev Front-End,</span>
             </div>
             <div>
-              Area:<span className="blue">Front-End</span>
+              Empresa:<span className="blue"> undefined</span>
             </div>
             {'\u007D'}
           </CodeItem>
